6e940ce2bff0394e064b632dadfe9505d90e6f94
dumpl1n <kiccassidy@gmail.com>
2024-02-03 22:37:14 -0500

    cleaned stg_customers model. split first_name, last_name.

--- codebase/northwinds_dbt/models/staging/stg_customers.sql
+++ codebase/northwinds_dbt/models/staging/stg_customers.sql
@@ -1,1 +1,16 @@
-select contact_name, address, phone from customers
+-- import CTE. similar to imports in python.
+WITH source AS (
+    SELECT *
+    FROM public.customers
+    ),
+
+-- logical CTE
+renamed AS (
+    SELECT customer_id, country,
+    split_part(contact_name, ' ', 1) AS first_name,
+    split_part(contact_name, ' ', 2) AS last_name
+    FROM source
+    )
+
+-- final SELECT statement
+    SELECT * FROM renamed
